3c30ebe1f9ccd4f1709f68379820087164314d0a
dinux-tech <75945307+dinux-tech@users.noreply.github.com>
2023-05-11 16:49:30 +0530

    RMI chat application code added

---
+++ README.md
@@ -0,0 +1,2 @@
+# RMI-Chat-Application
+This is a Java-based chat application that utilizes the Remote Method Invocation (RMI) protocol to allow users to communicate with each other in real-time.   (Tech Stack - Java, Hibernate, RMI, , MySQL)
---
+++ src/app/controller/Connection.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package app.controller;
+
+import org.hibernate.cfg.AnnotationConfiguration;
+import org.hibernate.SessionFactory;
+
+/*
+ *
+ * @author user
+ */
+public class Connection {
+
+    private static final SessionFactory sessionFactory;
+    
+    static {
+        try {
+            sessionFactory = new AnnotationConfiguration().configure().buildSessionFactory();
+        } catch (Throwable ex) {
+            System.err.println("Initial SessionFactory creation failed." + ex);
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+    public static SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
+}
